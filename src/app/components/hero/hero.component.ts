563dad50ac22339eff6ebd8e321b0374ed178bf7
menna-adel-10 <menna10_adel@hotmail.com>
2024-02-20 22:59:38 +0200

    .

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Data } from '../../../assets/data';
 import { CommonModule } from '@angular/common';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-hero',
@@ -12,6 +13,63 @@ import { CommonModule } from '@angular/common';
 export class HeroComponent {
 
   data = Data;
+  barChart: any = [];
+
+   constructor() {
+    Chart.register(...registerables)
+  }
+
+  ngOnInit(): void {
+    this.barChart = new Chart('canva', {
+      type: 'bar',
+      data: {
+        labels: [
+          'Eating',
+          'Drinking',
+          'Sleeping',
+          'Designing',
+          'Coding',
+          'Cycling',
+          'Running'
+        ],
+        datasets: [{
+          label: 'Activities',
+          barPercentage: 0.5,
+          barThickness: 20,
+          maxBarThickness: 15,
+          minBarLength: 0,
+
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            '#00d1de',
+            '#00d1de',
+            '#00d1de',
+            '#00d1de',
+            '#00d1de',
+            '#00d1de',
+            '#00d1de'
+          ],
+
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+
+          },
+          x: {
+
+            grid: {
+              offset: true
+            }
+          },
+        }
+      },
+    })
+
+  }
 
 
   getIconColor(index: number): string {
@@ -24,4 +82,6 @@ export class HeroComponent {
     }
 
   }
+
+
 }
